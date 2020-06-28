ca8339507f82df1d7604413e8ffb65be355d343f
tractors <huijianhua@163.com>
2020-06-28 23:26:30 +0800

    first commit

---
+++ app/src/main/java/com/will/navviewmodel/MasterFragment.java
@@ -0,0 +1,30 @@
+package com.will.navviewmodel;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MasterFragment} factory method to
+ * create an instance of this fragment.
+ */
+public class MasterFragment extends Fragment {
+
+
+    public MasterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_master, container, false);
+    }
+}
