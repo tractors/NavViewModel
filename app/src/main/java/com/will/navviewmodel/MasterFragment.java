c37c8649d61fcfdd5fa9de47fe5b9a533a9909b3
tractors <huijianhua@163.com>
2020-07-02 16:35:35 +0800

    master fragment add

--- app/src/main/java/com/will/navviewmodel/MasterFragment.java
+++ app/src/main/java/com/will/navviewmodel/MasterFragment.java
@@ -2,11 +2,18 @@ package com.will.navviewmodel;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.will.navviewmodel.databinding.FragmentMasterBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +31,22 @@ public class MasterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        MyViewModel myViewModel;
+        myViewModel = new ViewModelProvider(getActivity()).get(MyViewModel.class);
+        FragmentMasterBinding binding;
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_master,container,false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(getActivity());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController controller = Navigation.findNavController(view);
+                controller.navigate(R.id.action_masterFragment_to_detailFragment);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_master, container, false);
+        return binding.getRoot();
     }
 }
